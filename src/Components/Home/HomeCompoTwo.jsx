f6fb8593432f7d395e8b553dcae04a368e1c6476
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-05-25 08:19:44 -0700

    Latest

--- src/Components/Home/HomeCompoTwo.jsx
+++ src/Components/Home/HomeCompoTwo.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import icon1 from '../Images/icon.png'
 import icon2 from '../Images/icon1.png'
 import icon3 from '../Images/icon2.png'
+import hero2 from '../Images//hero2.png'
 
 const HomeCompoTwo = () => {
   return (
-    <div className='lg:py-20 py-10'>
-        <div className='flex lg:flex-row flex-col items-center justify-between lg:px-[13rem] px-5 lg:gap-5 gap-8'>
+    <div className=''>
+        <div className='flex lg:py-20 py-10 lg:flex-row flex-col items-center justify-between lg:px-[13rem] px-5 lg:gap-5 gap-8'>
             <div className='text-center shadow-md w-full py-5 lg:pb-14 pb-10'>
                 <div className='bg-neutral-100 p-8 mb-4 m-auto flex justify-center items-center w-fit rounded-full'>
                     <img src={icon2} alt="" className='lg:w-14 w-10'/>
@@ -34,6 +35,62 @@ const HomeCompoTwo = () => {
             </div>
 
 
+        </div>
+
+        <div className='flex lg:flex-row flex-col-reverse py-14 gap-10 lg:px-[13rem] px-6 items-center bg-neutral-100'>
+
+            <div className="join join-vertical lg:w-1/2 w-full">
+                <h2 className='mb-10'>WHY CHOOSE US</h2> 
+                <div className="collapse collapse-arrow join-item border border-base-300">
+                    <input type="radio" name="my-accordion-4" defaultChecked /> 
+                    <div className="collapse-title lg:text-lg text-md font-medium">
+                        Quick and easy hiring of creatives
+                    </div>
+                    <div className="collapse-content"> 
+                        <p className='text-xs leading-loose'>
+                            Looking for a quick and easy hiring process? Our platform 
+                            simplifies candidate sourcing, saving you time and resources. 
+                            Find the right talent swiftly and seamlessly, ensuring a smooth 
+                            hiring experience from start 
+                            to finish. Get started today and make hiring hassle-free!
+                        </p>
+                    </div>
+                </div>
+                
+                <div className="collapse collapse-arrow join-item border border-base-300">
+                    <input type="radio" name="my-accordion-4" /> 
+                    <div className="collapse-title lg:text-lg text-md font-medium">
+                        Reviews/Ratings on clients
+                    </div>
+                    <div className="collapse-content"> 
+                        <p className='text-xs leading-loose'>
+                            Choosing us means partnering with a team dedicated to your success. 
+                            Our commitment to excellence and customer satisfaction sets us apart. 
+                            Here are some reasons why our clients trust us:
+                        </p>
+                    </div>
+                </div>
+
+                <div className="collapse collapse-arrow join-item border border-base-300">
+                    <input type="radio" name="my-accordion-4" /> 
+                    <div className="collapse-title lg:text-lg text-md font-medium">
+                        Free onboarding as a Creative
+                    </div>
+                    <div className="collapse-content"> 
+                    <p className='text-xs leading-loose'>
+                            Looking for a quick and easy hiring process? Our platform 
+                            simplifies candidate sourcing, saving you time and resources. 
+                            Find the right talent swiftly and seamlessly, ensuring a smooth 
+                            hiring experience from start 
+                            to finish. Get started today and make hiring hassle-free!
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='lg:w-2/5 ml-auto w-full'>
+                <img src={hero2} alt="" className='w-full'/>
+            </div>
         </div>
     </div>
   )
