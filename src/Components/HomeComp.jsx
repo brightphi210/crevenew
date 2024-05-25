e6ebdee3a61c90c1d1498908b4ec31098248e2b6
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-05-25 06:49:50 -0700

    Latest

--- src/Components/HomeComp.jsx
+++ src/Components/HomeComp.jsx
@@ -4,40 +4,40 @@ import { FaArrowRightLong } from "react-icons/fa6";
 import { MdMoving } from "react-icons/md";
 const HomeComp = () => {
   return (
-    <div className='bg-black flex px-[13rem] py-[8rem] items-center h-full'>
+    <div className='bg-black lg:flex lg:px-[13rem] px-5 lg:py-[8rem] py-[7rem] items-center h-full'>
         
         <div className='w-full'>
-            <h2 className='text-5xl font-bold text-white'>HIRING MADE EASY !</h2>
-            <h3 className='text-3xl text-white py-5'>Hire an Expert or Be an Expert .</h3>
-            <p className='text-sm text-slate-300'> 
-                In the ever-evolving landscape of skills and knowledge, the choice between <br />
+            <h2 className='lg:text-5xl text-3xl font-bold text-white'>HIRING MADE EASY !</h2>
+            <h3 className='lg:text-3xl text-xl text-white py-5'>Hire an Expert or Be an Expert .</h3>
+            <p className='lg:text-sm text-xs text-slate-300 lg:w-10/12 w-full'> 
+                In the ever-evolving landscape of skills and knowledge, the choice between 
                 hiring an expert or becoming one yourself is a pivotal decision.
             </p>
             
-            <form action="" className='relative w-[30rem]' >
+            <form action="" className='relative lg:w-[30rem]' >
                 <input type="text" placeholder='Search here' 
-                    className='py-4 px-5 border-none bg-white mt-5 w-full  outline-none rounded-lg'
+                    className='py-4 px-5 border-none bg-white mt-5 w-full text-xs  outline-none rounded-lg'
                     required
                 />
 
-                <button className='rounde-lg bg-black text-white py-4 px-5 absolute right-2 top-6 rounded-lg'>
+                <button className='rounde-lg bg-black text-white py-3 px-4 absolute right-1 top-6 rounded-lg'>
                     <FaArrowRightLong />
                 </button>
 
             </form>
             <div className='pt-8'>
                 <p className='text-sm text-neutral-400 pb-3'>Popular Services</p>
-                <div className='flex text-white gap-3'>
+                <div className='lg:flex grid grid-cols-2 text-white gap-3'>
                     
-                    <button className='bg-neutral-900 w-[10rem] py-3 px-5 flex items-center rounded-md text-xs'>
+                    <button className='bg-neutral-900 lg:w-[10rem] w-full py-3 px-5 flex items-center rounded-md text-xs'>
                         Furniture <p className='flex  items-center justify-center bg-white p-2 text-black rounded-md ml-auto'><MdMoving /></p>
                     </button>
 
-                    <button className='bg-neutral-900 w-[10rem] py-3 px-5 flex items-center rounded-md text-xs'>
+                    <button className='bg-neutral-900 lg:w-[10rem] w-full py-3 px-5 flex items-center rounded-md text-xs'>
                         Web Design <p className='flex  items-center justify-center bg-white p-2 text-black rounded-md ml-auto'><MdMoving /></p>
                     </button>
 
-                    <button className='bg-neutral-900 w-[10rem] py-3 px-5 flex items-center rounded-md text-xs'>
+                    <button className='bg-neutral-900 lg:w-[10rem] w-full py-3 px-5 flex items-center rounded-md text-xs'>
                         Fashion <p className='flex  items-center justify-center bg-white p-2 text-black rounded-md ml-auto'><MdMoving /></p>
                     </button>
 
@@ -46,7 +46,7 @@ const HomeComp = () => {
         </div>
 
 
-        <div className='w-3/5 ml-auto'>
+        <div className='w-3/5 ml-auto lg:block hidden'>
             <img src={hero} alt="" className='w-full'/>
         </div>
     </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import logo from './Images/Creve.png'
+import logo1 from './Images/Creve1.png'
+import { MdPeople } from "react-icons/md";
 
 const Navbar = () => {
   return (
     <div className='py-10 bg-black' >
-        <div className='w-10/12 fixed bg-white left-0 right-0 flex m-auto py-5 px-10 rounded-lg'>
-            <div className='w-20'>
+        <div className='lg:w-10/12 w-11/12 fixed bg-white left-0 right-0 flex items-center m-auto py-3 lg:px-10 px-3 rounded-lg backdrop-filter backdrop-blur-lg bg-opacity-90'>
+            <div className='lg:block w-24 hidden'>
                 <img src={logo} alt="" className='cursor-pointer'/>
             </div>
 
-            <ul className='flex ml-auto gap-10 text-xs'>
+            <div className='lg:hidden w-10 block'>
+                <img src={logo1} alt="" className='cursor-pointer'/>
+            </div>
+
+            <ul className='flex items-center ml-auto lg:gap-10 gap-3 text-xs'>
                 <li className='cursor-pointer'>About</li>
-                <li className='cursor-pointer'>Hire Creatives</li>
+                <li className='cursor-pointer'>Hire Now</li>
                 <li className='cursor-pointer'>Login</li>
-                <li className='cursor-pointer'>Join</li>
+                <li className='cursor-pointer bg-black flex gap-2 items-center lg:py-3 lg:px-5 px-3 py-2 rounded-md text-white'>Join <MdPeople /></li>
             </ul>
         </div>
     </div>
