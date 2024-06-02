e124ec88e4fe8c9df8b31a5c2a88c326a8b9f6c7
Bright Philip <chibuzorphilip2001@gmail.com>
2024-06-02 11:58:12 -0700

    Latest

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './Images/Creve.png'
 import logo1 from './Images/Creve1.png'
 import { MdPeople } from "react-icons/md";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { jwtDecode } from "jwt-decode";
 
 const Navbar = () => {
+
+    const navigate = useNavigate()
+
+    const [token, setToken] = useState(() => localStorage.getItem('token') ? JSON.parse(localStorage.getItem('token')) : null);
+
+    const userToken = token?.access ? jwtDecode(token.access) : null;
+    console.log('This is the token', userToken);
+
+    const logout = async (e) => {
+        e.preventDefault()
+        setToken(null)
+        localStorage.removeItem('token')
+        navigate('/')
+    }
+
+
   return (
     <div className='' >
         <div className='lg:w-full z-10 fixed border border-slate-100 bg-white left-0 right-0 flex items-center m-auto py-3 lg:px-10 px-3 backdrop-filter backdrop-blur-3xl bg-opacity-80'>
@@ -22,13 +39,44 @@ const Navbar = () => {
 
             <ul className='flex items-center ml-auto lg:gap-10 gap-3 text-xs'>
                 <li className='cursor-pointer'>About</li>
-                <li className='cursor-pointer'>Hire Now</li>
-                <Link to={'/login'}>
-                    <li className='cursor-pointer'>Login</li>
-                </Link>
+
+                {userToken ? 
+                
+                    <li className='cursor-pointer' onClick={logout}>Logout</li> :
+
+                    <Link to={'/login'}>
+                        <li className='cursor-pointer'>Login</li>
+                    </Link>
+                
+                }
+              
+
+                {!userToken && 
                 <Link to={'/register'}>
                     <li className='cursor-pointer bg-black flex gap-2 items-center lg:py-3 lg:px-5 px-3 py-2 rounded-md text-white'>Join <MdPeople /></li>
                 </Link>
+                }
+
+
+                {userToken && (
+
+                
+                <>
+                {userToken.role === 'Creative' && 
+                    <Link to={'/creative-dashboard-home'}>
+                        <li className='cursor-pointer bg-black flex gap-2 items-center lg:py-3 lg:px-5 px-3 py-2 rounded-md text-white'>Dashboard</li>
+                    </Link>
+                }
+
+
+                {userToken.role === 'Client' && 
+                    <Link to={'/user-dashboard-home'}>
+                        <li className='cursor-pointer bg-black flex gap-2 items-center lg:py-3 lg:px-5 px-3 py-2 rounded-md text-white'>Dashboard</li>
+                    </Link>
+                }
+                </> 
+
+                )}
             </ul>
         </div>
     </div>
