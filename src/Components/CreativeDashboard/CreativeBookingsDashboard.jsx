93ead8863038bac7d8716dd9e45f9dcbfe60912d
Bright Philip <chibuzorphilip2001@gmail.com>
2024-06-25 03:45:09 -0700

    Latest

--- src/Components/CreativeDashboard/CreativeBookingsDashboard.jsx
+++ src/Components/CreativeDashboard/CreativeBookingsDashboard.jsx
@@ -6,6 +6,8 @@ import { Link } from 'react-router-dom'
 import prof from '../Images/Avatars.png'
 import { TbClick } from "react-icons/tb";
 import { BsThreeDots } from "react-icons/bs";
+import { LuSearch } from "react-icons/lu";
+import { BiMessageSquareDots } from "react-icons/bi";
 
 import data from '../Mock/data.json'
 
@@ -20,7 +22,7 @@ const CreativeBookingsDashboard = () => {
 
 
   return (
-    <div className='flex flex-row w-full bg-neutral-100'>
+    <div className='flex flex-row w-full bg-neutral-100 2xl:h-[100vh] xl:h-full lg:h-full'>
     <div className=''>
       <CreativeSideBarCom show={show} />
     </div>
@@ -51,140 +53,45 @@ export const CreativeBookings = () => {
   return (
     <div className='2xl:p-20 2xl:pt-28 2xl:pl-[18rem] lg:pl-[18rem] p-5 pt-20'>
       <div>
-        <div className=''>
+        <div className='flex lg:flex-row flex-col gap-4 items-center'>
+          <div className='w-full'>
             <h2>My Bookings</h2>
             <p className='text-xs pt-2'>List of all bookings from clients</p>
+          </div>
+
+          <div className='relative lg:w-1/2 w-full ml-auto'>
+            <input type="text" placeholder="Search Bookings" className="input px-5 text-xs input-bordered w-full rounded-full" />
+            <button className='absolute right-2 top-1 color text-white p-3 rounded-full '><LuSearch /></button>
+          </div>
         </div>
 
-        <div className='w-full flex 2xl:flex-row lg:flex-col-reverse flex-col-reverse mt-5 gap-10'>
+        <div className='w-full lg:mt-10 mt-5 pt-5 gap-10 overflow-y-scroll h-[70vh]'>
 
 
-          <div className='2xl:w-full w-full h-[40rem] overflow-y-scroll overflow-x-auto bg-white rounded-lg'>
+          <div className='2xl:w-full grid 2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-2 lg:gap-10 gap-4 w-full rounded-lg'>
+            
+            {data.map((eachData) =>(
 
-            {/* <div className='myGrid myGridtwo myGridthree  2xl:p-10 p-5 lg:grid-cols-2 grid grid-cols-1 gap-5 '>
+              <label className='bg-white p-5 rounded-md cursor-pointer' drawer-conten  htmlFor="my-drawer-4">
+                <div className='flex gap-3 items-center'>
+                  <div className='rounded-full w-10 h-10 overflow-hidden'>
+                    <img src={eachData.pics} className='w-full h-full ' alt="" />
+                  </div>
 
-              <div className='flex gap-5 items-center bg-white lg:w-full w-full p-6 rounded-md drop-shadow-sm' >
-                <div className='w-24'>
-                  <img src={prof} alt="" />
-                </div>
+                  <div>
+                    <h2 className='text-sm'>{eachData.name}</h2>
+                    <p className='text-xs'>{eachData.phone}</p>
+                  </div>
 
-                <div className='flex flex-col gap-1'>
-                  <h2 className='font-semibold'>John Doe</h2>
-                  <p className='text-xs'>johndoe@gmail.com</p>
-
-                  <div className='flex gap-2 pt-1'>
-                    <button className='mycolor3 text-[10px] px-5 py-2 font-medium rounded-md'>Contact</button>
-                    <button className='mycolor2 text-[10px] px-5 py-2 font-medium rounded-md text-white' onClick={handleShowBoookingsDesc}>View</button>
+                  <div className="drawer-content ml-auto">
+                    <label htmlFor="my-drawer-4" className="drawer-button hover:bg-accent btn  btn-primary min-h-[2rem] max-h-[2rem] mycolor2 border-none text-[10px] px-5  font-medium rounded-md text-white">View</label>
                   </div>
                 </div>
-              </div>
 
+                <p className='text-xs pt-3'>{eachData.message.slice(0, 20)} . . .</p>
+              </label>
+            ))}
 
-              <div className='flex gap-5 items-center bg-white lg:w-full w-full p-6 rounded-md drop-shadow-sm' >
-                <div className='w-24'>
-                  <img src={prof} alt="" />
-                </div>
-
-                <div className='flex flex-col gap-1'>
-                  <h2 className='font-semibold'>John Doe</h2>
-                  <p className='text-xs'>johndoe@gmail.com</p>
-
-                  <div className='flex gap-2 pt-1'>
-                    <button className='mycolor3 text-[10px] px-5 py-2 font-medium rounded-md'>Contact</button>
-                    <button className='mycolor2 text-[10px] px-5 py-2 font-medium rounded-md text-white' onClick={handleShowBoookingsDesc}>View</button>
-                  </div>
-                </div>
-              </div>
-
-              <div className='flex gap-5 items-center bg-white lg:w-full w-full p-6 rounded-md drop-shadow-sm' >
-                <div className='w-24'>
-                  <img src={prof} alt="" />
-                </div>
-
-                <div className='flex flex-col gap-1'>
-                  <h2 className='font-semibold'>John Doe</h2>
-                  <p className='text-xs'>johndoe@gmail.com</p>
-
-                  <div className='flex gap-2 pt-1'>
-                    <button className='mycolor3 text-[10px] px-5 py-2 font-medium rounded-md'>Contact</button>
-                    <button className='mycolor2 text-[10px] px-5 py-2 font-medium rounded-md text-white' onClick={handleShowBoookingsDesc}>View</button>
-                  </div>
-                </div>
-              </div>
-
-
-              <div className='flex gap-5 items-center bg-white lg:w-full w-full p-6 rounded-md drop-shadow-sm' >
-                <div className='w-24'>
-                  <img src={prof} alt="" />
-                </div>
-
-                <div className='flex flex-col gap-1'>
-                  <h2 className='font-semibold'>John Doe</h2>
-                  <p className='text-xs'>johndoe@gmail.com</p>
-
-                  <div className='flex gap-2 pt-1'>
-                    <button className='mycolor3 text-[10px] px-5 py-2 font-medium rounded-md'>Contact</button>
-                    <button className='mycolor2 text-[10px] px-5 py-2 font-medium rounded-md text-white' onClick={handleShowBoookingsDesc}>View</button>
-                  </div>
-                </div>
-              </div>
-
-            </div> */}
-
-            <div className="overflow-x-auto">
-              <table className="table">
-                {/* head */}
-                <thead>
-                  <tr>
-                    <th>Pics</th>
-                    <th>Name</th>
-                    <th>Message</th>
-                    <th>Time</th>
-                    <th>Action</th>
-                  </tr>
-                </thead>
-
-                <tbody>
-
-                  {console.log('This is ', data)}
-
-                  {data.map((eachData) => (
-                    <tr>
-
-                      <td>
-                        <div className="flex items-center gap-3">
-                          <div className="avatar">
-                            <div className="mask mask-squircle h-8 w-8">
-                              <img
-                                src={eachData.pics}
-                                alt="Avatar Tailwind CSS Component" />
-                            </div>
-                          </div>
-                        </div>
-                      </td>
-
-                      <td>
-                            <p className="text-xs">{eachData.name}</p>
-                      </td>
-
-
-                      <td className='text-xs'>{eachData.message.slice(0, 10)} . . . .</td>
-                      <td className='text-xs'>{eachData.time}</td>
-                      <td>
-                        {/* <button className='mycolor2 text-[10px] px-5 py-2 font-medium rounded-md text-white' onClick={handleShowBoookingsDesc}>View</button> */}
-                        <div className="drawer-content">
-                          <label htmlFor="my-drawer-4" className="drawer-button hover:bg-accent btn  btn-primary min-h-[2rem] max-h-[2rem] mycolor2 border-none text-[10px] px-5  font-medium rounded-md text-white">View</label>
-                        </div>
-                      </td>
-    
-                    </tr>
-                  ))}
-
-
-                </tbody>
-
-              </table>
-            </div>
           </div>
         </div>
       </div>
@@ -194,12 +101,37 @@ export const CreativeBookings = () => {
         <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
         <div className="drawer-side">
           <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
-          <ul className="menu bg-base-200 text-base-content min-h-full lg:w-[25%] w-[80%] p-4 pt-20">
-            <li><a>Sidebar Item 1</a></li>
-            <li><a>Sidebar Item 2</a></li>
+          <ul className="menu bg-base-200 text-base-content min-h-full lg:w-[25%] w-[85%] p-6 pt-32">
+            <div className='flex justify-center'>
+
+              <div className='text-center'>
+                  <div className='rounded-full flex justify-center m-auto w-20 h-20 overflow-hidden'>
+                    <img src="https://img.daisyui.com/tailwind-css-component-profile-2@56w.png" className='w-full h-full ' alt="" />
+                  </div>
+
+                  <div>
+                    <h2 className='text-sm font-bold pt-5'>John Doe</h2>
+                    <p className='text-xl font-bold text-neutral-500 py-3'>08094422799</p>
+
+                    <p className='lg:text-sm text-xs text-center lg:leading-[30px] leading-[28px] font-light'>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                      Neque earum impedit atque molestias vitae deserunt aliquid sint, 
+                      reiciendis ut ipsa, assumenda perferendis praesentium nostrum 
+                      fugit accusantium. Cupiditate, quae. Architecto, accusamus.
+                    </p>
+                  </div>
+
+                  <div className='mt-5 flex justify-center lg:gap-5 gap-3 lg:px-10 px-5 w-full'>
+                    <button className="btn lg:w-1/2 w-full btn-neutral text-xs  text-white min-h-[2.6rem] max-h-[2.6rem] ">Contact</button>
+                  </div>
+              </div>
+            </div>
           </ul>
         </div>
       </div>
+
+
+
 
       {/* {showBoookingsDesc === false ? 
 
