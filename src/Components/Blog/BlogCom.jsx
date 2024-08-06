0fd8f5e5d3a98b750e03b65efb2629e48660d042
Bright Philip <chibuzorphilip2001@gmail.com>
2024-08-06 19:52:53 +0100

    Latest

--- src/Components/Blog/BlogCom.jsx
+++ src/Components/Blog/BlogCom.jsx
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { BASE_URL } from '../Auth/BaseUrl'
 import { IoArrowForwardSharp } from "react-icons/io5";
+import { RxDotsHorizontal } from "react-icons/rx";
+import a from '../Images/blogImg/a1.jpg'
+import MyLoader from '../allLoadingState/MyLoader';
+
 
 const BlogCom = () => { 
 
     const [blogData, setBlogData] = useState([])
+    const [eachBlog, setEachBlog] = useState({})
     const [isLoading, setIsLoading] = useState(false)
+
 
     const url = `${BASE_URL}/blog/`
 
@@ -34,6 +40,7 @@ const BlogCom = () => {
 
 
     console.log('This is blog data', blogData);
+    console.log('This is each blog data', eachBlog);
   return (
     <div className='2xl:pt-40 pt-32 text-ce 2xl:px-[20rem] xl:px-[8rem] lg:px-[8rem] px-5'>
       <div className=''>
@@ -49,44 +56,77 @@ const BlogCom = () => {
       </div>
 
 
-      <div className='grid lg:grid-cols-3 md:grid-cols-1 grid-cols-1 gap-20 items-start 2xl:pt-28 pt-10'>
-        <div>
-            <h2 className='text-xl font-bold lg:pb-10 pb-5'>Choose what to read</h2>
-            <ul className='flex lg:flex-col flex-row flex-wrap  gap-3 pt-0 pl-5 border-l border-neutral-300'>
-                <li className='text-sm cursor-pointer pr-3 border-r border-neutral-300'>Company News</li>
-                <li className='text-sm cursor-pointer pr-3 border-r border-neutral-300'>Career & Success</li>
-                <li className='text-sm cursor-pointer pr-3 border-r border-neutral-300'>Community</li>
-                <li className='text-sm cursor-pointer pr-3 border-r border-neutral-300'>Product & Inovations</li>
-            </ul>
+        <div className='grid lg:grid-cols-3 md:grid-cols-1 grid-cols-1 gap-20 items-start 2xl:pt-28 pt-10'>
+                <div>
+                    <h2 className='text-xl font-bold lg:pb-10 pb-5'>Choose what to read</h2>
+                    <ul className='flex lg:flex-col flex-row flex-wrap  gap-3 pt-0 pl-5 border-l border-neutral-300'>
+                        <li className='text-sm cursor-pointer pr-3 border-r border-neutral-300'>Company News</li>
+                        <li className='text-sm cursor-pointer pr-3 border-r border-neutral-300'>Career & Success</li>
+                        <li className='text-sm cursor-pointer pr-3 border-r border-neutral-300'>Community</li>
+                        <li className='text-sm cursor-pointer pr-3 border-r border-neutral-300'>Product & Inovations</li>
+                    </ul>
+                </div>
+
+            {isLoading === true ? 
+                <div className='text-center  pb-10'>
+                    <span class="loader3"></span>
+                </div>
+            : 
+                <div className='col-span-2 lg:ml-auto cursor-pointer'>
+                    {blogData.map((blog)=>(
+                        <div className='flex lg:flex-row flex-col  lg:gap-10 gap-4 pb-5 mb-20' onClick={()=>{document.getElementById('my_modal_3').showModal(); setEachBlog(blog)}}>
+                            <div className='w-full  border border-neutral-200 2xl:h-[18rem] xl:h-[18rem] lg:h-[18rem]  h-[15rem] md:h-[30rem] overflow-hidden rounded-xl'>
+                                <img src={blog?.image} alt="" className='w-full h-full rounded-xl object-cover'/>
+                            </div>
+
+                            <div className='lg:w-[80%] w-full'>
+                                <p className='text-xs text-neutral-400 pb-3'>29.Jun.2021</p>
+                                <h2 className='2xl:text-xl xl:text-base font-semibold'>{blog?.title}</h2>
+                                <p className='text-sm py-5 text-justify'>{blog?.description.slice(0, 150)}   .  .  .</p>
+                                <p className='text-sm text-neutral-600'>By: {blog?.author}</p>
+                                <button onClick={()=>{document.getElementById('my_modal_3').showModal(); setEachBlog(blog)}} className='bg-white-800 text-black border border-neutral-300 lg:w-fit w-full lg:py-3 py-3 lg:px-6 mt-5 rounded-full text-sm flex justify-center gap-2 items-center'>
+                                    View Blog <IoArrowForwardSharp />
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            }
         </div>
 
-        {isLoading === true ? 
-            <div className='col-span-2 flex lg:flex-row flex-col items-center lg:gap-10 gap-3 w-full mb-10'>
-                <div className='bg-neutral-300 rounded-xl w-[100%] h-[18rem]'></div>
-                <div className='bg-neutral-300 rounded-xl lg:w-[70%] w-full lg:h-[18rem] h-[6rem]'></div>
-            </div>
-        : 
-            <div className='col-span-2 lg:ml-auto cursor-pointer'>
-                {blogData.map((blog)=>(
-                    <div className='flex lg:flex-row flex-col  lg:gap-10 gap-4 pb-5'>
-                        <div className='w-full  border border-neutral-200 2xl:h-[18rem] xl:h-[18rem] lg:h-[18rem]  h-[15rem] md:h-[30rem] overflow-hidden rounded-xl'>
-                            <img src={blog?.image} alt="" className='w-full h-full rounded-xl object-cover'/>
+
+        <dialog id="my_modal_3" className="modal">
+            <div className="modal-box lg:max-w-[90%] w-[98%] lg:max-h-[98%] max-h-[96%] rounded-md lg:p-20 2xl:px-[25rem] xl:px-[15rem] lg:px-[15rem] p-5 py-10">
+                <form method="dialog">
+                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                </form>
+
+                <div className='flex w-full justify-center items-center m-auto'>
+                    <div className=''>
+                        <div className=' mb-5 pb-3 '>
+                            <h2 className='lg:text-4xl text-2xl font-semibold pb-3 text-green-900'>{eachBlog?.title}</h2>
+                            <p className='text-sm'>Author : <span className='font-bold'>{eachBlog?.author}</span></p>
                         </div>
 
-                        <div className='lg:w-[80%] w-full'>
-                            <p className='text-xs text-neutral-400 pb-3'>29.Jun.2021</p>
-                            <h2 className='2xl:text-xl xl:text-base font-semibold'>{blog?.title}</h2>
-                            <p className='text-xs py-5 text-justify'>{blog?.description.slice(0, 200)}   .  .  .</p>
-                            <p className='text-sm text-neutral-600'>By: {blog?.author}</p>
-                            <button className='bg-black text-white lg:w-fit w-full py-3 lg:px-6 mt-5 rounded-full text-sm flex gap-2 items-center'>
-                                View Blog <IoArrowForwardSharp />
-                            </button>
+                        <div className='w-full overflow-hidden lg:h-[30rem] h-[20%] rounded-lg border border-neutral-200'>
+                            <img src={eachBlog?.image} alt="" className='w-full h-full rounded-lg object-cover'/>
+                        </div>
+
+                        <div className='flex items-center gap-3 pt-10 pb-5' >
+                            <h3 className='text-green-600 underline cursor-pointer'>Blog</h3>
+                            <p className='text-sm font-bold'>{eachBlog?.title}</p>
+                        </div>
+
+                        <p className=' lg:leading-[40px] leading-[30px] lg:text-base text-sm text-justify'>{eachBlog?.description}</p>
+                        <div className='flex items-center gap-2 mt-6 pt-4 border-t border-neutral-300'>
+                            <button className='bg-green-900 text-sm lg:w-[30%] w-full  text-white py-3 px-6 rounded-full'>Share</button>
+                            <button onClick={()=>document.getElementById('my_modal_3').close()} className='border border-neutral-300 text-sm lg:w-[20%] w-full text-gray-800 py-3 px-6 rounded-full'>Close</button>
                         </div>
                     </div>
-                ))}
+                </div>
+
             </div>
-        }
-      </div>
+        </dialog>
     </div>
   )
 }
