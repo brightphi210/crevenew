80db8e81603c82b661ecddd289f7d4d14a5a7aca
Bright Philip <chibuzorphilip2001@gmail.com>
2024-08-10 14:34:25 +0100

    Latest

--- src/Components/UserDashboard/UserNavbar.jsx
+++ src/Components/UserDashboard/UserNavbar.jsx
@@ -65,7 +65,7 @@ const UserNavbar = ({handleShow, show}) => {
     <div>
         <div className={'flex flex-row items-center shadow-sm fixed right-0 left-0 bg-white py-4 lg:px-20 px-5  z-50 backdrop-filter backdrop-blur-3xl bg-opacity-80'}>
 
-            <div className='cursor-pointer lg:block hidden'>
+            <div className='cursor-pointer mr-5'>
             <Link to={'/'}>
                 <img src={logo} alt="" className='w-7'/>
             </Link>
